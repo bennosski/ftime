c41c61f45c4768fcde8e7446996a3ce4fb6572de
Ben <bln@stanford.edu>
2019-05-02 18:50:49 -0700

    license

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
     ],
 )
 
--- test/test.py
+++ test/test.py
@@ -1,17 +1,15 @@
 import time
 from profiler import timed
 
-@timed
-def f(x):
-    time.sleep(0.1)
-    return x
 
 @timed
-def g(x):
-    time.sleep(0.2)
-    return x**2
+def f():
+    print('hi')
 
+@timed
+def g():
+    print('there')
 
-f(1)
-f(2)
-g(3)
+f()
+f()
+g()
