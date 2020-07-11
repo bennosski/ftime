35270597a9933c66ac73b15ffe1a0db0975f98c5
Ben <bennosski@gmail.com>
2020-07-10 22:24:37 -0700

    fixed no function calls case fixed runs taking zero time error removed dependence on atexit (not working for Windows)

--- profile/profiler.py
+++ profile/profiler.py
@@ -1,19 +1,21 @@
 import time
-import atexit
+
+# todo:
+# work better with longer function names (forced abbreviation?)
 
 class timed:
-    program_start_time = time.time() # assumes profiler is imported at start of code
+    program_start_time = time.time()
     functions = {}
 
     def __init__(self, f):
         self.f = f
-        if len(timed.functions)==0:
-            atexit.register(self._profile)
         timed.functions[f.__name__] = {'times': []}
+
 
     def __get__(self, instance, _):
         self.obj = instance
         return self.__call__
+
 
     def __call__(self, *args, **kwargs):
         t0 = time.time()
@@ -24,33 +26,40 @@ class timed:
         timed.functions[self.f.__name__]['times'].append(time.time()-t0)
         return out
 
-    def _profile(self):
+
+    def results():
         program_run_time = time.time() - timed.program_start_time
 
         fnames = []
         totals = []
         ncalls = []
         total_total = 0
+            
+        if program_run_time == 0:
+            max_name_len    = 4
+            max_ncall_len   = 1
+            max_percent_len = 4
+        else:
+            for fname in timed.functions:
+                fnames.append(fname)
+                times = timed.functions[fname]['times']
+                total = sum(times)
+                if len(times)==0:
+                    tavg = 0
+                else:
+                    tavg = total/len(times)
+                total_total += total
+                totals.append(total)
+                ncalls.append(len(times))
+                     
+            totals,fnames,ncalls = zip(*sorted(zip(totals,fnames,ncalls), reverse=True))
+    
+            percents = [100.0*t/program_run_time for t in totals]
+            
+            max_name_len    = max(4, max(len(x) for x in fnames))
+            max_ncall_len   = max(len(str(n)) for n in ncalls)
+            max_percent_len = max(len('{:.1f}'.format(p)) for p in percents)
 
-        for fname in timed.functions:
-            fnames.append(fname)
-            times = timed.functions[fname]['times']
-            total = sum(times)
-            if len(times)==0:
-                tavg = 0
-            else:
-                tavg = total/len(times)
-            total_total += total
-            totals.append(total)
-            ncalls.append(len(times))
-                 
-        totals,fnames,ncalls = zip(*sorted(zip(totals,fnames,ncalls), reverse=True))
-
-        percents = [100.0*t/program_run_time for t in totals]
-        
-        max_name_len    = max(4, max(len(x) for x in fnames))
-        max_ncall_len   = max(len(str(n)) for n in ncalls)
-        max_percent_len = max(len('{:.1f}'.format(p)) for p in percents)
 
         space = max_name_len + max_ncall_len + max_percent_len + 36
         s1 = (space-15)//2
